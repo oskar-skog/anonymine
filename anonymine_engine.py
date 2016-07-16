a6f717a5a7c0a6bec204ad1476c2abb74d161383
Oskar Skog <oskar.skog.finland@gmail.com>
2016-07-16 22:27:30 +0300

    preferred encdoing may be ASCCI while used encoding is ISO-8859-1 [FreeBSD on VM]

--- anonymine_engine.py
+++ anonymine_engine.py
@@ -252,7 +252,17 @@ class hiscores():
                     'Nickname'
                 )
                 if sys.version_info[0] == 2:
-                    nick = nick.decode(locale.getpreferredencoding())
+                    encodings = [
+                        locale.getpreferredencoding(),  # SHOULD be correct.
+                        'utf-8',                        # Always worth trying.
+                        'iso-8859-1'                    # Will not fail.
+                    ]
+                    for encoding in encodings:
+                        try:
+                            nick = nick.decode(locale.getpreferredencoding())
+                            break
+                        except:
+                            pass
         # Load the list again (inputfunction may take a very long time)
         # and add the nickname to the entry.
         new_entry[4] = nick
