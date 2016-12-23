352926e3a586b8c0346b40fcb6eb4a34f8dac1ec
Oskar Skog <oskar.skog.finland@gmail.com>
2016-12-23 18:38:46 +0200

    configure.py: typo/syntax error

--- configure.py
+++ configure.py
@@ -393,7 +393,7 @@ def find_MODULES(Makefile, flags):
             if paths:
                 # NOTICE:  Just notice this rather ugly construction.
                 # You can't just change the data.
-                if paths[0].beginswith(libdir)
+                if paths[0].beginswith(libdir):
                     Makefile['MODULES'] = '$(libdir)' + paths[0][len(libdir):]
                 else:
                     Makefile['MODULES'] = '$(prefix)' + paths[0][len(prefix):]
